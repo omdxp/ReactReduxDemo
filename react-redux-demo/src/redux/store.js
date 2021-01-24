d23f15801c878a72071906d190957cd5a40c0450
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-01-24 16:04:55 +0100

    redux-devtools-extension added

--- react-redux-demo/src/redux/store.js
+++ react-redux-demo/src/redux/store.js
@@ -1,12 +1,17 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import cakeReducer from "./cake/cakeReducer";
 import { iceCreamReducer } from "./iceCream/iceCreamReducer";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 export const rootReducer = combineReducers({
   cake: cakeReducer,
   iceCream: iceCreamReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 export default store;
